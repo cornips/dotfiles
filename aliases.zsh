79355d0900e14fede6a1f40e5053a7a6cfe8a1ab
Martijn Cornips <cornips@users.noreply.github.com>
2023-01-12 16:12:48 +0100

    switch ssh rsa to ed25519

--- aliases.zsh
+++ aliases.zsh
@@ -1,9 +1,9 @@
 # Shortcuts
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
-alias ssh-server1="ssh -t -i ~/.ssh/id_rsa mcornips@server1.cornipshosting.nl -p 61619 'cd \/home\/mcornips\/html && exec \$SHELL -l'"
-alias ssh-heldro="ssh -t -i ~/.ssh/id_rsa heldro@verkoop.heldro.nl 'cd \/home\/heldro\/public_html && exec \$SHELL -l'"
+alias ssh-server1="ssh -t -i ~/.ssh/id_ed25519 mcornips@server1.cornipshosting.nl -p 61619 'cd \/home\/mcornips\/html && exec \$SHELL -l'"
+alias ssh-heldro="ssh -t -i ~/.ssh/id_ed25519 heldro@verkoop.heldro.nl 'cd \/home\/heldro\/public_html && exec \$SHELL -l'"
 alias dirsize="du -sch"
-alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
+alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 ccat() {
   pygmentize -P style=monokai -g $1 | less
--- ssh.sh
+++ ssh.sh
@@ -4,17 +4,19 @@ echo "Generating a new SSH key for GitHub/GitLab/Bitbucket..."
 
 # Generating a new SSH key
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key
-ssh-keygen -t rsa -C $1 -f ~/.ssh/id_rsa
+ssh-keygen -t ed25519 -C $1 -f ~/.ssh/id_ed25519
 
 # Adding your SSH key to the ssh-agent
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
 eval "$(ssh-agent -s)"
 
 touch ~/.ssh/config
-echo "Host server1.cornipshosting.nl\n Port 61619\n\nHost *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_rsa\n\n# Fig ssh integration. Keep at the bottom of this file.\nMatch all\n  Include ~/.fig/ssh" | tee ~/.ssh/config
+echo "Host server1.cornipshosting.nl\n Port 61619\n\nHost *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed25519\n\n# Fig ssh integration. Keep at the bottom of this file.\nMatch all\n  Include ~/.fig/ssh" | tee ~/.ssh/config > /dev/null
 
-ssh-add -K ~/.ssh/id_rsa
+ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+
+echo # Whitespace
 
 # Adding your SSH key to your GitHub account
 # https://docs.github.com/en/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account
-echo "run 'pbcopy < ~/.ssh/id_rsa.pub' and paste that into GitHub/GitLab/Bitbucket"
+echo "run 'pbcopy < ~/.ssh/id_ed25519.pub' and paste that into GitHub/GitLab/Bitbucket"
